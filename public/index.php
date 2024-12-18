c2bda5338b3fec690d68a4f9e1d20dc420619496
Abdelkarim Moussi <abdelkarimmoussi73@gmail.com>
2024-12-19 00:00:53 +0100

    fichier des diagrames ERD et USE CASE

--- public/index.php
+++ public/index.php
@@ -11,6 +11,42 @@
     </div>
 </header>
 
+<section class="grid lg:grid-cols-2 bg-black text-white py-10 px-6">
+<div>
+<h1 class=" text-3xl font-semibold uppercase py-5">Who we are</h1>
+    <p class="pb-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, repudiandae. Consequatur repudiandae nisi, excepturi similique officia ipsam tempora modi quos?</p>
+    <a href="signin.php" class="text-md uppercase tracking-wide font-semibold py-2 px-5 border border-md border-white hover:border-green-600 rounded-md">login</a>
+</div>    
+</section>
+<section class="py-10 px-6">
+    <h1 class="text-center text-3xl font-semibold uppercase py-6">What we can do for you ?</h1>
+    <div class="grid grid-cols-2 lg:grid-cols-4 grid gap-5">
+        <div class="border border-green-600 px-3 py-4 flex flex-col items-center justify-center gap-3 rounded-md">
+            <h4 class="font-bold uppercase">excutives & employees</h4>
+            <div class="w-[40px] h-[40px] bg-green-600 rounded-md"></div>
+        </div>
+        <div class="border border-green-600 px-3 py-4 flex flex-col items-center justify-center gap-3 rounded-md">
+            <h4 class="font-bold uppercase">Sexual Harassment</h4>
+            <div class="w-[40px] h-[40px] bg-green-600 rounded-md"></div>
+        </div>
+        <div class="border border-green-600 px-3 py-4 flex flex-col items-center justify-center gap-3 rounded-md">
+            <h4 class="font-bold uppercase">Equal Pay</h4>
+            <div class="w-[40px] h-[40px] bg-green-600 rounded-md"></div>
+        </div>
+        <div class="border border-green-600 px-3 py-4 flex flex-col items-center justify-center gap-3 rounded-md">
+            <h4 class="font-bold uppercase">Litigation</h4>
+            <div class="w-[40px] h-[40px] bg-green-600 rounded-md"></div>
+        </div>
+        <div class="col-start-2 border border-green-600 px-3 py-4 flex flex-col items-center justify-center gap-3 rounded-md">
+            <h4 class="font-bold uppercase">Employment Agreements</h4>
+            <div class="w-[40px] h-[40px] bg-green-600 rounded-md"></div>
+        </div>
+        <div class="border border-green-600 px-3 py-4 flex flex-col items-center justify-center gap-3 rounded-md">
+            <h4 class="font-bold uppercase">Counseling</h4>
+            <div class="w-[40px] h-[40px] bg-green-600 rounded-md"></div>
+        </div>
+    </div>
+</section>
 <script src="../public/assets/js/app.js?v=<?php echo time(); ?>"></script>
 </body>
 </html>
